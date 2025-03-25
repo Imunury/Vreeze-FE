dd5d4e3a7c093e5b6d1d59c6ff1374e5fd9118ee
Imunury <dipblu0808@gmail.com>
2025-03-25 14:41:28 +0900

    make voice reading

--- lib/screens/my_subscribe_screen.dart
+++ lib/screens/my_subscribe_screen.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 
-class MySubscribeScreen extends StatefulWidget {
-  const MySubscribeScreen({super.key});
-
-  @override
-  _MySubscribeScreenState createState() => _MySubscribeScreenState();
-}
-
-class _MySubscribeScreenState extends State<MySubscribeScreen> {
+class MySubscribeScreen extends StatelessWidget {
+  final List<String> imageUrls = [
+    'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg',
+    'https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8JTIzaW1hZ2V8ZW58MHx8MHx8fDA%3D',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFYqoKTu_o3Zns2yExbst2Co84Gpc2Q1RJbA&s',
+    'https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return SizedBox(
+      height: 300,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            imageUrls[index], // imageUrls 리스트 사용
+            fit: BoxFit.cover,
+          );
+        },
+        itemCount: imageUrls.length, // 리스트 길이만큼 설정
+        viewportFraction: 0.8,
+        scale: 0.9,
+        pagination: SwiperPagination(),
+        control: SwiperControl(),
+      ),
     );
   }
 }
--- lib/screens/record_voice_screen.dart
+++ lib/screens/record_voice_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 class RecordVoiceScreen extends StatefulWidget {
@@ -8,10 +10,115 @@ class RecordVoiceScreen extends StatefulWidget {
 }
 
 class _RecordVoiceScreenState extends State<RecordVoiceScreen> {
+  List<Map<String, String>> allTexts = [];
+  List<Map<String, String>> displayedTexts = [];
+  int currentPage = 1;
+  final int itemsPerPage = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    loadTexts();
+  }
+
+  Future<void> loadTexts() async {
+    try {
+      String data = await rootBundle.loadString('assets/utils/text1.json');
+      List<dynamic> jsonList = json.decode(data);
+
+      List<Map<String, String>> parsedTexts = jsonList
+          .where((item) =>
+      int.tryParse(item["ID"] ?? '0') != null &&
+          int.parse(item["ID"]) <= 100)
+          .map<Map<String, String>>((item) => {
+        "ID": item["ID"].toString(),
+        "text": item["text"].toString(),
+      })
+          .toList();
+
+      setState(() {
+        allTexts = parsedTexts;
+        updateDisplayedTexts();
+      });
+    } catch (e) {
+      debugPrint("Error loading JSON: $e");
+    }
+  }
+
+  void updateDisplayedTexts() {
+    int startIndex = (currentPage - 1) * itemsPerPage;
+    int endIndex = startIndex + itemsPerPage;
+    setState(() {
+      displayedTexts = allTexts.sublist(
+          startIndex, endIndex > allTexts.length ? allTexts.length : endIndex);
+    });
+  }
+
+  void nextPage() {
+    if (currentPage * itemsPerPage < allTexts.length) {
+      setState(() {
+        currentPage++;
+        updateDisplayedTexts();
+      });
+    }
+  }
+
+  void previousPage() {
+    if (currentPage > 1) {
+      setState(() {
+        currentPage--;
+        updateDisplayedTexts();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ListView(
+            children: displayedTexts
+                .map((item) => Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, top: 5, bottom: 5),
+              child: Text(
+                '${item["ID"]}. ${item["text"]}',
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                  letterSpacing: -0.5,
+                ),
+                textAlign: TextAlign.left,
+                softWrap: true,
+              ),
+            ))
+                .toList(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: currentPage > 1 ? previousPage : null,
+                child: const Text("이전"),
+              ),
+              Text(
+                "$currentPage / ${allTexts.length ~/ itemsPerPage}",
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              ElevatedButton(
+                onPressed:
+                currentPage * itemsPerPage < allTexts.length ? nextPage : null,
+                child: const Text("다음"),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
