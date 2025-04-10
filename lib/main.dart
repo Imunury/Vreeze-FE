8868b2be188daacd854479e3ec24b57cc5751dfa
LeeJS9856 <wnstj1057@naver.com>
2025-04-10 22:45:56 +0900

    chore : 라이브러리 추가 * .env 관리를 위해 dotenv 라이브러리 추가 * .env git ignore로 관리 * jwt decoder 라이브러리 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,19 @@ import 'package:breeze/screens/delete_account_screen.dart';
 import 'package:breeze/screens/profile_screen.dart';
 import 'package:breeze/screens/setting_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:breeze/screens/chat_screen.dart';
 import 'package:breeze/screens/chat_voice_screen.dart';
 import 'package:breeze/screens/make_voice_screen.dart';
 import 'package:breeze/screens/my_subscribe_screen.dart';
 import 'package:breeze/screens/record_voice_screen.dart';
 import 'package:breeze/screens/login_screen.dart';
-
 import 'MainLayout.dart';
+import 'package:breeze/cores/dio_client.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
+  setupDio();
   runApp(const MyApp());
 }
 
@@ -30,9 +33,9 @@ class MyApp extends StatelessWidget {
         '/my_subscribe': (context) => MainLayout(body: MySubscribeScreen()),
         '/make_voice': (context) => MainLayout(body: MakeVoiceScreen()),
         '/record_voice': (context) => MainLayout(body: RecordVoiceScreen()),
-        '/setting' : (context) => MainLayout(body: SettingScreen()),
-        '/profile' : (context) => MainLayout(body: ProfileScreen()),
-        '/delete_account' : (context) => MainLayout(body: DeleteAccountScreen()),
+        '/setting': (context) => MainLayout(body: SettingScreen()),
+        '/profile': (context) => MainLayout(body: ProfileScreen()),
+        '/delete_account': (context) => MainLayout(body: DeleteAccountScreen()),
       },
     );
   }
